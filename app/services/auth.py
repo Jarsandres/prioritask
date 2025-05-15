40be86b178e174db04277355a3a07331683460f1
Jarsandres <jarsandres@outlook.es>
2025-05-16 00:53:33 +0200

    ..

--- app/services/auth.py
+++ app/services/auth.py
@@ -4,7 +4,7 @@ from jose import jwt
 from passlib.context import CryptContext
 
 from app.models.user import Usuario
-from app.db.session import get_session, async_session
+from app.db.session import async_session
 
 pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
