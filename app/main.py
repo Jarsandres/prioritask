0ea265f0f146106bd936a8c970407351c1e7273d
Jarsandres <jarsandres@outlook.es>
2025-05-28 16:56:04 +0200

    Mejora la documentación del main agregando contacto y licencia de la API

--- app/main.py
+++ app/main.py
@@ -4,7 +4,15 @@ from app.api.v1 import api_router
 app = FastAPI(
     title="Prioritask API",
     version="1.0",
-    description="Gestión inteligente de tareas con IA. Esta API permite a los usuarios gestionar tareas de manera eficiente, incluyendo la creación, actualización, eliminación y asignación de tareas. Además, ofrece funcionalidades avanzadas como la priorización, agrupación y reformulación de tareas utilizando inteligencia artificial."
+    description="Gestión inteligente de tareas con IA. Esta API permite a los usuarios gestionar tareas de manera eficiente, incluyendo la creación, actualización, eliminación y asignación de tareas. Además, ofrece funcionalidades avanzadas como la priorización, agrupación y reformulación de tareas utilizando inteligencia artificial.",
+    contact={
+        "name": "Equipo Prioritask",
+        "email": "soporte@prioritask.com"
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT"
+    }
 )
 
 @app.get("/")
