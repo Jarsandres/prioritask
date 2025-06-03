dce766604c23b894c55b51a84f366a01aa630fb4
Joel Rios <joel.rios@cloudappi.net>
2025-06-03 11:38:45 +0200

    Eliminar test_main

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_read_root():
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert resp.json() == {"message": "¡Prioritask está funcionando!"}
